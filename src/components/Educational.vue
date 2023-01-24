24644e795445a8fe2d1b67849dd590cc6c7993cc
MeowMerry <meow3525@gmail.com>
2023-01-24 02:16:43 -0600

    updated

--- src/components/Educational.vue
+++ src/components/Educational.vue
@@ -58,9 +58,9 @@
           </div>
           <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="img/code.png" alt />
-              <h4>CODECAMP#4 SOFTWARE PARK THAILAND</h4>
-              <p class="text-muted">Full Stack Web Developer | Oct 2019 - Mar 2019</p>
+              <img class="mx-auto rounded-circle" src="https://www.youtube.com/watch?v=hjF2-74c7XI" alt />
+              <h4>Public Talks :</h4>
+              <p class="text-muted">“Application Security” Jeeny and Bractlet Speaker Series</p>
               <ul class="list-inline social-buttons">
                 <li class="list-inline-item">
                   <a href="#">
@@ -85,7 +85,9 @@
           <div class="col-lg-8 mx-auto text-center">
             <p
               class="large text-muted"
-            >I am a proficient in overseeing sales operations willing to change a field of work and seeking a full-time position in the field of web development. I am a fast learner, hard worker and team player, hopefully my benefit could work for your company . SelfImprovement and Friendly. Speak less and listen more with none-stop learning.</p>
+            >Professional software engineer of experience contributing to the design and software development.
+I enjoy the frontend aspect of design which focuses on visuals and client needs.
+Excellent capabilities identifying potential issues and implementing product solutions. Strong problem solving and organizational skills useful in any multidisciplinary setting. Effective in a team atmosphere with thoughtful attention to communication.</p>
           </div>
         </div>
       </div>
--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -16,23 +16,47 @@
               </div>
               <div class="timeline-panel">
                 <div class="timeline-heading">
-                  <h4>Dec 2020 - Present</h4>
+                  <h4>Aug 2022 - Dec 2022</h4>
+                  <h3 class="subheading">Gigamon</h3>
+                  <h5>Software Engineer</h5>
+                </div>
+                <div class="timeline-body">
+                  <ul class="b">
+                    <li>Created an intuitive column profile to allow users to easily change columns and choose appropriate column profiles with
+Material UI, CSS, React and TypeScript</li>
+                    <li>Developed the state management infrastructure using Redux Toolkit to preserve predictable state management change.</li>
+                    <li>Utilized Material UI to modernize design on insight web page following UI/UX best practices and to improve user functionality
+and end user feedback while interacting with the application.</li>
+                    <li>Migrated codebase from class component to function component to enhance code readability and to use React Hook in order
+to prepare for rapid scaling and optimized debugging and maintainability of code.</li>
+                    <li>Modularized components to increase code comprehension and reusability.</li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="timeline-image">
+                <img class="rounded-circle img-fluid" src="img/about/1.jpg" alt />
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4>Dec 2020 - Aug 2022</h4>
                   <h3 class="subheading">Level-11 company</h3>
                   <h5>Software Engineer</h5>
                 </div>
                 <div class="timeline-body">
                   <ul class="b">
-                    <li>Listening and understanding the customer requirements</li>
-                    <li>Analyzing customer needs and designing a technical solution</li>
-                    <li>Developing a layout of the application and UI/UX designing</li>
-                    <li>Coding, testing, and implementing commits, builds, and updates in React JS environment</li>
-                    <li>Developing and maintaining Git repository and establishing a great collaboration among the
-                      cross-functional development team</li>
-                    <li>Dividing the task into small parts and engaging with the entire development team</li>
-                    <li>Implemented websites and lading page from concept through deployment</li>
-                    <li>Implemented websites and lading page from concept through deployment</li>
-                    <li>Built the logic for a streamlined ad-serving platform that scaled</li>
-                    <li>Educational institutions and online classroom management</li>
+                    <li>Utilized React with Typescript creating a user customizable hedge fund app via Next.js SSR technology with dynamic,
+server-rendered content decreasing management overhead.</li>
+                    <li>Leveraged React to migrate a multi-page user experience into a single page to improve customer engagement.</li>
+                    <li>Developed application features to provide a central hub for employees to collaborate across the organization with file sharing,
+discussion groups announcements.</li>
+                    <li>Created multiple designs using Storybook, allowing a team to build UIs in isolation in the application.</li>
+                    <li>Employed Next.js dynamic client-side routing, integrating public and private routes for user-controlled access while
+standardizing the behavior and application structure resulting in decreasing server calls.</li>
+                    <li>Created middleware functions in Golang to handle endpoint requests to modularize backend requests with a discernible chain
+of responsibility and separation of concern.</li>
                   </ul>
                 </div>
               </div>
@@ -49,9 +73,15 @@
                 </div>
                 <div class="timeline-body">
                   <ul class="b">
-                    <li>Implemented websites and lading page from concept through deployment</li>
-                    <li>Built the logic for a streamlined ad-serving platform that scaled</li>
-                    <li>Educational institutions and online classroom management</li>
+                    <li>Built iEnergyWhiz, a real-time energy big data analytics and visualization platform with integration to automatic demand
+response program with React to take advantage of the immutable, unidirectional data flow and rendering UI components with
+data-heavy state management.</li>
+                    <li>Utilized React Hooks and Context API for accessible state management to support the state of functional components
+influenced by asynchronous data fetching as a measure to prevent unnecessary rendering.</li>
+                    <li>Collaborate cross-functionally with the design team and back end team to develop the front end application data flow.</li>
+                    <li>Leveraged Redux to preserve predictable state management changes using the observer pattern of nested data sets and
+micro-actions across the app’s components for efficient debugging.</li>
+                    <li>Conducted code peer reviews with teammates to ensure software solution elevated client side experience.</li>
                   </ul>
                 </div>
               </div>
@@ -62,40 +92,21 @@
               </div>
               <div class="timeline-panel">
                 <div class="timeline-heading">
-                  <h4>Sept 2018 - Dec 2018</h4>
-                  <h3 class="subheading">ALORICA URUGUAY</h3>
-                  <h5>Amazon call center representative for USA</h5>
+                  <h4>2022</h4>
+                  <h3 class="subheading">OPEN SOURCE | Sveltool Chrome DevTool</h3>
+                  <h5>Software Engineer</h5>
                 </div>
                 <div class="timeline-body">
                   <ul class="b">
-                    <li>Manage large amounts of inbound and outbound calls in a timely manner</li>
-                    <li>Identify customers’ needs, clarify information, research every issue and provide solutions
-                      and/or alternatives</li>
+                   <li>Developed a Chrome DevTool for Svelte with Chrome Manifest Version 3 to locate and parse incoming resources, walking the resulting abstract syntax tree to isolate web page components, algorithmically extracting state and props.</li>
+                   <li>Used D3.js to create SVGs displaying dependency trees, component hierarchies, component rendering for simple and organized application visualization during the developing and debugging process for dev users.</li>
+                   <li>Achieved Contributors and Navbar features for the Svletool website using WebGL to display details and information.</li>
+                   <li>Product developed and maintained under tech accelerator OS Labs (http://opensourcelabs.io).</li>
                   </ul>
                 </div>
               </div>
             </li>
-            <li class="timeline-inverted">
-              <div class="timeline-image">
-                <img class="rounded-circle img-fluid" src="img/about/2.jpg" alt />
-              </div>
-              <div class="timeline-panel">
-                <div class="timeline-heading">
-                  <h4>Jan 2015 - June 2017</h4>
-                  <h3 class="subheading">MASTER PAC - ASIA</h3>
-                  <h5>Export Sale</h5>
-                </div>
-
-                <div class="timeline-body">
-                  <ul class="b">
-                    <li>Finding new customers and service our customer</li>
-                    <li>In charge of overall export sales operations</li>
-                    <li>Train and supervise employees</li>
-                    <li>Onsite with engineer team, visit customer on oversea, Laos, Vietnam, Cambodia, Myanmar</li>
-                  </ul>
-                </div>
-              </div>
-            </li>
+            
             <li class="timeline-inverted">
               <div class="timeline-image">
                 <h4>
